f36f371934767fbc8473aca1a3ea10e6c8ccec7c
Randy Taylor <randyisbusy@gmail.com>
2019-08-17 17:09:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,7 +36,7 @@ end
   def buy_dog(dog_name)
     new_dog = Dog.new(dog_name, self)
   end
-  def walk_dog
+  def walk_dogs
     perros = Dog.all.find_all{|pet| pet.owner == self}
     binding.pry
     perros
