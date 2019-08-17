5c147bd6f753dc053d94ba2a607a8a7ba76cfb92
Randy Taylor <randyisbusy@gmail.com>
2019-08-17 16:39:04 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,14 @@
 class Cat
   attr_accessor :owner
+  attr_reader :name
+  @@all = []
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  def self.all 
+    @@all 
   end
 end
